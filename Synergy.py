0c406d315434cb91e280da924691005466c28e09
Evgeniy Ivanov <irelion_synergy@mail.ru>
2023-11-06 16:23:46 +0300

    P-0000: init

---
+++ Synergy.py
@@ -0,0 +1,15 @@
+print('Введите количество вводимых числе от 1 до 10000:')
+numberArray = []
+NUMBER_LIMIT = 10000
+n = int(input('N = '))
+if n > NUMBER_LIMIT: 
+    print('Неверное количество символов')
+else:
+     for i in range(n):
+          number = abs(int(input(f'Число #{i + 1} = ')))
+          if number > NUMBER_LIMIT: 
+               print('Превышен допустимы диапазон')
+               break
+          numberArray.append(number)
+
+print(list(reversed(numberArray)))
